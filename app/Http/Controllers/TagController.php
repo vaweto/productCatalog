7ae7864c3cae6fd07f46c984ab72957ee147bfb2
vaweto <vagelisbisbikis@gmail.com>
2023-07-02 16:20:01 +0300

    fix Tag misspeling

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Tags;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
-class TagsController extends Controller
+class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -26,7 +26,7 @@ class TagsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Tags $tags)
+    public function show(Tag $tags)
     {
         //
     }
@@ -34,7 +34,7 @@ class TagsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tags $tags)
+    public function update(Request $request, Tag $tags)
     {
         //
     }
@@ -42,7 +42,7 @@ class TagsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tags $tags)
+    public function destroy(Tag $tags)
     {
         //
     }
