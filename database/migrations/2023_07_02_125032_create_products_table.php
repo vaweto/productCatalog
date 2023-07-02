5f2e1a8977d3d242f75f6d32c8ecea6261e251d8
vaweto <vagelisbisbikis@gmail.com>
2023-07-02 16:15:34 +0300

    make migrations for the models

--- database/migrations/2023_07_02_125032_create_products_table.php
+++ database/migrations/2023_07_02_125032_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->foreignIdFor(Category::class)->nullable()->constrained();
+            $table->float('price','10',2);
+            $table->dateTime('released_at')->nullable();
             $table->timestamps();
         });
     }
@@ -23,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2023_07_02_131209_create_taggables_table.php
+++ database/migrations/2023_07_02_131209_create_taggables_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('taggables', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId("tag_id")->constrained();
+            $table->morphs('taggable');
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('taggables');
     }
 };
